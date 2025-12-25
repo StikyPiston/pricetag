6afea0a7f56415407931adac446a5cadce3f42ff
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-25 18:05:09 +0200

    feat: Helper functions

--- Sources/pricetag/main.swift
+++ Sources/pricetag/main.swift
@@ -2,7 +2,6 @@ import Foundation
 
 // Database type
 struct PricetagDB: Codable {
-    var tags: [String]
     var paths: [String: [String]]
 }
 
@@ -25,7 +24,9 @@ func ensureFileExists(at url: URL, defaultContents: String = "[]") {
 
 ensureFileExists(at: dbDir)
 
-func loadDB(from url: URL) throws -> PricetagDB {
+// Load/Save database
+func loadDB() throws -> PricetagDB {
+    let url = dbDir
     guard FileManager.default.fileExists(atPath: url.path) else {
         return PricetagDB(tags: [], paths: [:])
     }
@@ -33,7 +34,40 @@ func loadDB(from url: URL) throws -> PricetagDB {
     return try JSONDecoder().decode(PricetagDB.self, from: data)
 }
 
-func saveDB(_ db: PricetagDB, to url: URL) throws {
+func saveDB(_ db: PricetagDB) throws {
     let data = try JSONEncoder().encode(db)
-    try data.write(to: url, options: .atomic)
+    try data.write(to: dbDir, options: .atomic)
+}
+
+// Tag a file
+func tagFile(file: String, tag: String) throws {
+    var db = loadDB()
+    db.paths[file].append(tag)
+    try saveDB(db: db)
+}
+
+// Remove tag from file
+func untagFile(from file: String, tag: String) {
+    var db = loadDB()
+    guard var tags = db.paths[file] else {
+        print("No tags for file \(file)")
+        return
+    }
+
+    // Remove the tag
+    tags.removeAll { $0 == tag }
+
+    if tags.isEmpty {
+        // Remove the file entry if no tags left
+        db.paths.removeValue(forKey: file)
+    } else {
+        db.paths[file] = tags
+    }
+}
+
+// Clear file tags
+func clearFile(file: String) {
+    var db = loadDB()
+    db.paths[file] = []
+    try saveDB(db: db)
 }
