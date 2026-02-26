c12560a24b02e44101091e74efe79bcfacbb90bd
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-26 12:17:41 +0200

    feat: tag new command

---
+++ cmd/tagnew.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/stikypiston/pricetag/internal"
+)
+
+// newCmd represents the new command
+var newCmd = &cobra.Command{
+	Use:   "new <name> <color>",
+	Short: "Create a new tag",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		color := internal.TagColor(args[1])
+
+		if !color.IsValid() {
+			return fmt.Errorf("Invalid color: %s", color)
+		}
+
+		db, path, err := internal.LoadDB()
+		if err != nil {
+			return err
+		}
+
+		if err := db.CreateTag(name, color); err != nil {
+			return err
+		}
+
+		return internal.SaveDB(db, path)
+	},
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) == 1 {
+			return internal.ValidColors, cobra.ShellCompDirectiveNoFileComp
+		}
+		return nil, cobra.ShellCompDirectiveDefault
+	},
+}
+
+func init() {
+	tagCmd.AddCommand(newCmd)
+}
