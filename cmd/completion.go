5f712f75869d8c2b4d6629a29ef16bfc2c3391e8
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-26 12:36:09 +0200

    feat: completion functions

---
+++ cmd/completion.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/stikypiston/pricetag/internal"
+)
+
+func completeTags(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	db, _, err := internal.LoadDB()
+	if err != nil {
+		return nil, cobra.ShellCompDirectiveDefault
+	}
+
+	var tags []string
+	for name := range db.Tags {
+		tags = append(tags, name)
+	}
+
+	return tags, cobra.ShellCompDirectiveNoFileComp
+}
+
+func completeFiles(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	return nil, cobra.ShellCompDirectiveDefault
+}
